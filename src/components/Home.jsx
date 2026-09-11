43bdda69e1d07e15f2c927713e5e62a6af4fb52b
basii199 <ubokabasi135@gmail.com>
2026-09-11 20:14:47 +0100

    fix: fix footer width

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,19 @@
-/* import React, { useState, useEffect } from 'react'
-import Hero from './Hero'
-import FeaturedProducts from './FeaturedProducts'
-import FeaturedCategories from './FeaturedCategories'
+import Hero from "./Hero";
+import FeaturedProducts from "./FeaturedProducts";
+import FeaturedCategories from "./FeaturedCategories";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
-    <div className="pb-20">
-      <Hero />
-      <FeaturedCategories />
-      <FeaturedProducts />
-    </div>
-  )
-}
-
-export default Home */
-
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Hero from './Hero'
-import FeaturedProducts from './FeaturedProducts'
-import FeaturedCategories from './FeaturedCategories'
-import Testimonials from './Testimonials'
-import Newsletter from './Newsletter'
-import Footer from './Footer'
-
-const Home = () => {
-  return (
-    <div className="">
+    <>
       <Hero />
       <FeaturedCategories />
       <FeaturedProducts />
       <Testimonials />
       <Footer />
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import Header from './Header'
-import { Outlet } from 'react-router-dom'
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
@@ -8,7 +7,7 @@ const Layout = () => {
       <Header />
       <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
